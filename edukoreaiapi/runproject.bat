c231007b3f5dc82de9fd8106562732e91324fbe9
Rajesh <asr.rajesh@gmail.com>
2026-09-12 12:54:17 +0530

    Env file added

--- edukoreaiapi/runproject.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\Rajesh\School\edukoreaiapi
-call C:\Rajesh\School\edukoreaiapi\.venv\Scripts\activate
-python main.py
--- edukoreaiui/runproject.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\Rajesh\School\edukoreaiui
-call C:\Rajesh\School\edukoreaiui\venv\Scripts\activate
-python main.py
